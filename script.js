d40e2be29a6d02b1884f32ec72f3954bd5a4a5a5
Nat Hsu <natalie.d.hsu@gmail.com>
2019-04-12 00:49:34 -0400

    added some JS

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const PROMPTS = [
-"How are you doing?",
-"What is the meaning of life?",
-];
+var prompts =
+    ["How are you doing?",
+        "What is the meaning of life?",
+        "How do you spend your free time?"];
 
-const p = PROMPTS.length;
-
-function randomPrompt(num){
-
+// getRandomInt: Nat -> Nat
+// produces random number from 0 to given Nat.
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 };
+
+// randomPrompt: Array -> String
+// returns a String from random index of array.
+function randomPrompt(array) {
+    return array[getRandomInt(array.length)]
+};
+
+function setQuestion(){
+    document.getElementById("questionHolder").placeholder = "hi";
+}
+
+console.log(randomPrompt(prompts));
